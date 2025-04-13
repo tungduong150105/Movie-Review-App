36310a1c53090152115273881f2970a5863f2e23
Huy Vu <23520657@gm.uit.edu.vn>
2025-04-13 21:24:04 +0700

    feat: add click handler for Buttons
    Add click handler for Buttons in SignUpActivity

--- app/src/main/java/com/example/moviereviewapp/Activities/SignupActivity.java
+++ app/src/main/java/com/example/moviereviewapp/Activities/SignupActivity.java
@@ -1,6 +1,9 @@
 package com.example.moviereviewapp.Activities;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -29,5 +32,45 @@ public class SignupActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editTextFirstAndLastName_SignUp = findViewById(R.id.editTextFirstAndLastName_SignUp);
+        editTextEmail_SignUp = findViewById(R.id.editTextEmail_SignUp);
+        editTextPassword_SignUp = findViewById(R.id.editTextPassword_SignUp);
+        editTextRe_enterPassword_SignUp = findViewById(R.id.editTextRe_enterPassword_SignUp);
+        checkBoxShowPass_SignUp = findViewById(R.id.checkBoxShowPass_SignUp);
+        btnCreateAccount_SignUp = findViewById(R.id.btnCreateAccount_SignUp);
+        btnSignIn_SignUp = findViewById(R.id.btnSignIn_SignUp);
+
+        checkBoxShowPass_SignUp.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Typeface currentTypeface = editTextPassword_SignUp.getTypeface();
+
+            if (isChecked) {
+                editTextPassword_SignUp.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                editTextRe_enterPassword_SignUp.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            } else {
+                editTextPassword_SignUp.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                editTextRe_enterPassword_SignUp.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
+
+            editTextPassword_SignUp.setTypeface(currentTypeface);
+            editTextRe_enterPassword_SignUp.setTypeface(currentTypeface);
+
+            editTextRe_enterPassword_SignUp.setSelection(editTextRe_enterPassword_SignUp.getText().length());
+        });
+
+        //ToDo: Xử lý đăng ký tài khoản
+    }
+    public void onClick_CreateAccount_SignUp(View view) {
+        String firstName = editTextFirstAndLastName_SignUp.getText().toString();
+        String email = editTextEmail_SignUp.getText().toString();
+        String password = editTextPassword_SignUp.getText().toString();
+        String reEnterPassword = editTextRe_enterPassword_SignUp.getText().toString();
+        //ToDo: Xử lý đăng ký tài khoản
+
+    }
+
+    public void onClick_SignIn_SignUp(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
